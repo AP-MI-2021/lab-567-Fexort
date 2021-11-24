379503eeb54fb7b2d681a8389d5039a294a22125
unknown <opreamatei82@ymail.com>
2021-11-24 18:42:25 +0200

    commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     lista = []
     runAllTests()
     while True:
-        option = input("pentru meniu apasati 1, pentru consola apasati 2; pentru a inchide apasai x: ")
+        option = input("pentru meniu apasati 1, pentru consola apasati 2 ; pentru a inchide apasai x: ")
         if option == "1":
              runMenu([])
         elif option == "2":
